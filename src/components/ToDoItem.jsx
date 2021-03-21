294217c8c9646d0be4e422fa3a0d1b5616ab4598
Mansi-singla <mansi1690.cse18@chitkara.edu.in>
2021-03-22 00:18:31 +0530

    my project11

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+
+function ToDoItem(props){
+
+
+const [isDone,setisDone]=useState(false);
+  function handleClick(){
+    setisDone(prevValue =>{
+      return !prevValue;
+    });
+
+    
+  }
+  return(
+  <div onClick={handleClick}>
+    <li style= {{textDecoration:isDone?"line-through":"none"}} >{props.text}</li>
+  </div>
+);
+}
+
+export default ToDoItem;
